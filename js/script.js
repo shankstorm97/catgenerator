30e24342dcc30fc6c2cac6c4ea3fc7b0a47adc9e
Shanky singh <shankysingh@Shankys-MacBook-Air.local>
2022-01-31 14:36:19 +0530

    website hosting

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function catGenerator() {
+    var image = document.createElement('img');
+    var div = document.getElementById('cat-gen');
+    image.src = "/project cat/images/Culture-Grumpy-Cat-487386121.jpeg";
+    div.appendChild(image);
+}
